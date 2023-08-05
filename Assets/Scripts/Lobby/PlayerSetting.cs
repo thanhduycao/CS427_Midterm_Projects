e8c1ab29f031635ea4412192d2437c67a653a1b5
vietnhatthai <thaivietnhat02@gmail.com>
2023-08-05 11:55:22 +0700

    feat: add random name

--- Assets/Scripts/Lobby/PlayerSetting.cs
+++ Assets/Scripts/Lobby/PlayerSetting.cs
@@ -9,17 +9,17 @@ public class PlayerSetting : MonoBehaviour
     [SerializeField] private GameObject _colorPrefab;
     [SerializeField] private TMPro.TMP_InputField _playerNameInputField;
     [SerializeField] private GridLayoutGroup _colorGridLayoutGroup;
+    [SerializeField] private RawImage _colorPreview;
 
     private void Awake()
     {
+        _colorPreview.color = CurrenPlayerData.Instance.GetColor();
         // clear all prefabs
         foreach (Transform child in _colorGridLayoutGroup.transform)
         {
             Destroy(child.gameObject);
         }
 
-        // add new prefabs
-        // get color from Constants.Colors
         foreach (var color in Constants.Colors)
         {
             var colorPrefab = Instantiate(_colorPrefab, _colorGridLayoutGroup.transform);
@@ -27,31 +27,23 @@ public class PlayerSetting : MonoBehaviour
             rawImage.color = color;
             colorPrefab.GetComponent<Button>().onClick.AddListener(() =>
             {
-                PlayerPrefs.SetString("PlayerColor", color.ToString());
                 _lobbyOrchestrator.SetColor(color: color);
+                _colorPreview.color = color;
                 CurrenPlayerData.Instance.SetColor(color);
             });
         }
-
         // set default
-        _lobbyOrchestrator.SetName(PlayerPrefs.GetString("PlayerName", "Player"));
-        string colorString = PlayerPrefs.GetString("PlayerColor", Color.white.ToString());
-        try
-        {
-            ColorUtility.TryParseHtmlString(colorString, out Color _color);
-            _lobbyOrchestrator.SetColor(color: _color);
-        }
-        catch (System.Exception) { }
+        _lobbyOrchestrator.SetName(NVJOBNameGen.Uppercase(NVJOBNameGen.GiveAName(7)));
+        _lobbyOrchestrator.SetColor(color: Color.white);
     }
 
     private void Start()
     {
-        _playerNameInputField.text = PlayerPrefs.GetString("PlayerName", "Player");
+        _playerNameInputField.text = NVJOBNameGen.Uppercase(NVJOBNameGen.GiveAName(7));
     }
 
     public void OnPlayerNameEntered()
     {
-        PlayerPrefs.SetString("PlayerName", _playerNameInputField.text);
         CurrenPlayerData.Instance.SetName(_playerNameInputField.text);
         _lobbyOrchestrator.SetName(name: _playerNameInputField.text);
     }
