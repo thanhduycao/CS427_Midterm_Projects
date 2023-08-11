46b23b071defeef0387360803a9f8fc60e94ac00
vietnhatthai <thaivietnhat02@gmail.com>
2023-08-11 18:29:46 +0700

    feat: add `Round3`

--- Assets/Scripts/Models/Constants.cs
+++ Assets/Scripts/Models/Constants.cs
@@ -6,6 +6,6 @@ public class Constants {
     public const string RoundKey = "r";
     public const string ColorKey = "c";
 
-    public static readonly List<string> Rounds = new() { "Round1", "Round2" };
+    public static readonly List<string> Rounds = new() { "Round1", "Round2", "Round3" };
     public static readonly List<Color> Colors = new() { Color.red, Color.blue, Color.green, Color.yellow, Color.magenta, Color.cyan };
 }
