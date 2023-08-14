628e865dbfade7b7209dc2a6547060553f2a3175
Nam Tran Minh <81914645+trminhnam@users.noreply.github.com>
2023-08-14 21:59:46 +0900

    fix screen

--- Assets/Scripts/Managers/ScreenManager.cs
+++ Assets/Scripts/Managers/ScreenManager.cs
@@ -4,6 +4,12 @@ public class ScreenManager : MonoBehaviour
 {
     void Start()
     {
+        SetDefaultScreenResolution();
+    }
+
+    // Extracted function to set the default screen resolution
+    private void SetDefaultScreenResolution()
+    {
         Screen.SetResolution(1080, 720, false);
     }
 }
