3c554e82282f90d817b8383fc714f1ce6d67a37c
Nam Tran Minh <81914645+trminhnam@users.noreply.github.com>
2023-08-14 22:03:00 +0900

    fix: sound manager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,152 +1,157 @@
-// Import required namespaces
+// Import necessary Unity libraries.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-// Define the SoundManager class
+// SoundManager class responsible for managing game sounds.
 public class SoundManager : MonoBehaviour
 {
-    // Singleton instance
     private static SoundManager _instance;
-
-    // Array of sound data
+    
+    // Array of SoundData objects that define different sounds in the game.
     public SoundData[] sounds;
-
-    // Dictionary to store sound cooldown timers
+    
+    // Dictionary to keep track of sound cooldowns.
     private static Dictionary<SoundData.Sound, float> soundTimerDictionary;
-
-    // Reference to the background audio source
+    
+    // Reference to the background music audio source.
     [SerializeField] private AudioSource backgroundSource;
-
-    // Lists to store music and sound effect audio sources
+    
+    // Lists to store music and sound effect audio sources.
     private List<AudioSource> musicSource = new List<AudioSource>();
     private List<AudioSource> sfxSource = new List<AudioSource>();
-
-    // Variables for music and sound effect volumes
+    
+    // Volume settings for music and sound effects.
     private float _musicVolume = 1;
     private float _sfxVolume = 1;
 
-    // Singleton property
+    // Singleton instance of SoundManager.
     public static SoundManager instance
     {
         get; private set;
     }
 
-    // Awake method executed when the script is loaded
+    // Awake is called before the Start method.
     private void Awake()
     {
-        // Singleton check
+        // Singleton pattern: Ensure only one instance of SoundManager exists.
         if (instance != null)
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); // Destroy duplicate instances.
         }
         else
         {
-            // Initialize singleton instance and prevent it from being destroyed on scene changes
             instance = this;
-            DontDestroyOnLoad(this.gameObject);
-
-            // Initialize the soundTimerDictionary
+            DontDestroyOnLoad(this.gameObject); // Persist across scene changes.
             soundTimerDictionary = new Dictionary<SoundData.Sound, float>();
-
-            // Loop through sound data and set up audio sources
-            foreach (SoundData sound in sounds)
-            {
-                if (sound.source == null)
-                {
-                    sound.source = gameObject.AddComponent<AudioSource>();
-                }
-                sound.source.clip = sound.audioClip;
-
-                sound.source.volume = sound.volume;
-                sound.source.pitch = sound.pitch;
-                sound.source.loop = sound.isLoop;
-
-                // Set up sound cooldown if applicable
-                if (sound.hasCooldown)
-                {
-                    soundTimerDictionary[sound.sound] = 0f;
-                }
-
-                // Categorize audio sources as music or sound effects
-                if (sound.isMusicSound == true)
-                {
-                    musicSource.Add(sound.source);
-                }
-                else
-                {
-                    sfxSource.Add(sound.source);
-                }
-            }
+            InitializeSoundSources(); // Initialize audio sources from SoundData objects.
         }
     }
 
-    // Start method executed on the first frame
+    // Start is called before the first frame update.
     private void Start()
     {
-        // Initialize button sound and play the main menu sound
+        InitializeButtonSound(); // Set up button click sounds.
+        PlayMainMenuSound(); // Play main menu sound.
+    }
+
+    // Initialize audio sources based on SoundData objects.
+    private void InitializeSoundSources()
+    {
+        foreach (SoundData sound in sounds)
+        {
+            InitializeSound(sound);
+            CategorizeSoundSource(sound);
+        }
+    }
+
+    // Initialize properties of an audio source based on a SoundData object.
+    private void InitializeSound(SoundData sound)
+    {
+        if (sound.source == null)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+        }
+        sound.source.clip = sound.audioClip;
+
+        sound.source.volume = sound.volume;
+        sound.source.pitch = sound.pitch;
+        sound.source.loop = sound.isLoop;
+
+        if (sound.hasCooldown)
+        {
+            soundTimerDictionary[sound.sound] = 0f;
+        }
+    }
+
+    // Categorize an audio source as music or sound effect based on SoundData.
+    private void CategorizeSoundSource(SoundData sound)
+    {
+        if (sound.isMusicSound)
+        {
+            musicSource.Add(sound.source);
+        }
+        else
+        {
+            sfxSource.Add(sound.source);
+        }
+    }
+
+    // Play the main menu sound.
+    private void PlayMainMenuSound()
+    {
         InitializeButtonSound();
         Play(SoundData.Sound.MainMenu);
     }
 
-    // Play a sound by name
+    // Play a sound based on its name.
     public void Play(SoundData.Sound soundName)
     {
         SoundData sound = System.Array.Find(sounds, s => s.sound == soundName);
-
         if (sound == null)
         {
             Debug.LogError("Sound " + name + " Not Found!");
             return;
         }
-
-        if (!CanPlaySound(sound)) return;
-
-        // Play the selected sound
-        sound.source.Play();
+        if (!CanPlaySound(sound)) return; // Check sound cooldown.
+        sound.source.Play(); // Play the sound.
     }
 
-    // Stop a sound by name
+    // Stop playing a sound based on its name.
     public void Stop(SoundData.Sound soundName)
     {
         SoundData sound = System.Array.Find(sounds, s => s.sound == soundName);
-
         if (sound == null)
         {
             Debug.LogError("Sound " + name + " Not Found!");
             return;
         }
-
-        // Stop the selected sound
-        sound.source.Stop();
+        sound.source.Stop(); // Stop the sound.
     }
 
-    // Check if a sound can be played based on cooldown
+    // Check if a sound can be played based on its cooldown.
     private static bool CanPlaySound(SoundData sound)
     {
         if (soundTimerDictionary.ContainsKey(sound.sound))
         {
             float lastTimePlayed = soundTimerDictionary[sound.sound];
-
             if (lastTimePlayed + sound.audioClip.length < Time.time)
             {
                 soundTimerDictionary[sound.sound] = Time.time;
                 return true;
             }
-
             return false;
         }
-
         return true;
     }
 
-    // Called when a new level is loaded
+    // Called when a new level is loaded.
     private void OnLevelWasLoaded(int level)
     {
-        // Initialize button sound and handle scene-specific audio
         InitializeButtonSound();
+        // Play appropriate sounds based on the loaded level.
         switch (level)
         {
             case 3:
@@ -168,62 +173,67 @@ public class SoundManager : MonoBehaviour
         Debug.Log(level);
     }
 
-    // Attach button click sound to all buttons in the scene
+    // Set up button click sounds for all buttons.
     private void InitializeButtonSound()
     {
         Button[] buttons = FindObjectsOfType<Button>();
-
         foreach (Button button in buttons)
         {
+            // Add a click sound to each button.
             button.onClick.AddListener(() => Play(SoundData.Sound.ButtonClick));
         }
     }
 
-    // Toggle music playback
+    // Toggle music on/off.
     public void ToggleMusic()
     {
-        foreach (AudioSource source in musicSource)
-        {
-            source.mute = !source.mute;
-        }
+        ToggleAudioSources(musicSource);
     }
 
-    // Toggle sound effect playback
+    // Toggle sound effects on/off.
     public void ToggleSFX()
     {
-        foreach (AudioSource source in sfxSource)
+        ToggleAudioSources(sfxSource);
+    }
+
+    // Toggle audio sources on/off.
+    private void ToggleAudioSources(List<AudioSource> sources)
+    {
+        foreach (AudioSource source in sources)
         {
             source.mute = !source.mute;
         }
     }
 
-    // Change music volume
+    // Change the volume of music.
     public void ChangeMusicVolume(float volume)
     {
-        _musicVolume = volume;
-        foreach (AudioSource source in musicSource)
-        {
-            source.volume = volume;
-        }
+        ChangeVolume(musicSource, volume, ref _musicVolume);
     }
 
-    // Change sound effect volume
+    // Change the volume of sound effects.
     public void ChangeSFXVolume(float volume)
     {
-        _sfxVolume = volume;
-        foreach (AudioSource source in sfxSource)
+        ChangeVolume(sfxSource, volume, ref _sfxVolume);
+    }
+
+    // Change the volume of audio sources.
+    private void ChangeVolume(List<AudioSource> sources, float volume, ref float targetVolume)
+    {
+        targetVolume = volume;
+        foreach (AudioSource source in sources)
         {
             source.volume = volume;
         }
     }
 
-    // Get current music volume
+    // Get the current music volume setting.
     public float GetMusicVolume()
     {
         return _musicVolume;
     }
 
-    // Get current sound effect volume
+    // Get the current sound effects volume setting.
     public float GetSFXVolume()
     {
         return _sfxVolume;
