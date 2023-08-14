993e992777cb3c57bd7a3e4f09d44fc916be16b0
Nam Tran Minh <81914645+trminhnam@users.noreply.github.com>
2023-08-14 21:57:26 +0900

    fix "here"

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,37 +1,57 @@
+// Import required namespaces
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+// Define the SoundManager class
 public class SoundManager : MonoBehaviour
 {
+    // Singleton instance
     private static SoundManager _instance;
+
+    // Array of sound data
     public SoundData[] sounds;
+
+    // Dictionary to store sound cooldown timers
     private static Dictionary<SoundData.Sound, float> soundTimerDictionary;
+
+    // Reference to the background audio source
     [SerializeField] private AudioSource backgroundSource;
+
+    // Lists to store music and sound effect audio sources
     private List<AudioSource> musicSource = new List<AudioSource>();
     private List<AudioSource> sfxSource = new List<AudioSource>();
+
+    // Variables for music and sound effect volumes
     private float _musicVolume = 1;
     private float _sfxVolume = 1;
 
+    // Singleton property
     public static SoundManager instance
     {
         get; private set;
     }
 
+    // Awake method executed when the script is loaded
     private void Awake()
     {
+        // Singleton check
         if (instance != null)
         {
             Destroy(this.gameObject);
         }
         else
         {
+            // Initialize singleton instance and prevent it from being destroyed on scene changes
             instance = this;
             DontDestroyOnLoad(this.gameObject);
+
+            // Initialize the soundTimerDictionary
             soundTimerDictionary = new Dictionary<SoundData.Sound, float>();
 
+            // Loop through sound data and set up audio sources
             foreach (SoundData sound in sounds)
             {
                 if (sound.source == null)
@@ -44,11 +64,13 @@ public class SoundManager : MonoBehaviour
                 sound.source.pitch = sound.pitch;
                 sound.source.loop = sound.isLoop;
 
+                // Set up sound cooldown if applicable
                 if (sound.hasCooldown)
                 {
-                    Debug.Log(sound.sound);
                     soundTimerDictionary[sound.sound] = 0f;
                 }
+
+                // Categorize audio sources as music or sound effects
                 if (sound.isMusicSound == true)
                 {
                     musicSource.Add(sound.source);
@@ -61,13 +83,15 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    // Start method executed on the first frame
     private void Start()
     {
-        // Add this part after having a theme song
+        // Initialize button sound and play the main menu sound
         InitializeButtonSound();
         Play(SoundData.Sound.MainMenu);
-
     }
+
+    // Play a sound by name
     public void Play(SoundData.Sound soundName)
     {
         SoundData sound = System.Array.Find(sounds, s => s.sound == soundName);
@@ -80,10 +104,11 @@ public class SoundManager : MonoBehaviour
 
         if (!CanPlaySound(sound)) return;
 
-        Debug.Log("Here");
+        // Play the selected sound
         sound.source.Play();
     }
 
+    // Stop a sound by name
     public void Stop(SoundData.Sound soundName)
     {
         SoundData sound = System.Array.Find(sounds, s => s.sound == soundName);
@@ -94,9 +119,11 @@ public class SoundManager : MonoBehaviour
             return;
         }
 
+        // Stop the selected sound
         sound.source.Stop();
     }
 
+    // Check if a sound can be played based on cooldown
     private static bool CanPlaySound(SoundData sound)
     {
         if (soundTimerDictionary.ContainsKey(sound.sound))
@@ -115,10 +142,10 @@ public class SoundManager : MonoBehaviour
         return true;
     }
 
+    // Called when a new level is loaded
     private void OnLevelWasLoaded(int level)
     {
-        // Scene currentScene = SceneManager.GetActiveScene();
-        // int sceneIndex = currentScene.buildIndex;
+        // Initialize button sound and handle scene-specific audio
         InitializeButtonSound();
         switch (level)
         {
@@ -141,28 +168,27 @@ public class SoundManager : MonoBehaviour
         Debug.Log(level);
     }
 
+    // Attach button click sound to all buttons in the scene
     private void InitializeButtonSound()
     {
-        // Find all Button components in the scene
         Button[] buttons = FindObjectsOfType<Button>();
 
-        // Attach the audio playback to each button's onClick event
         foreach (Button button in buttons)
         {
             button.onClick.AddListener(() => Play(SoundData.Sound.ButtonClick));
         }
-
     }
 
+    // Toggle music playback
     public void ToggleMusic()
     {
         foreach (AudioSource source in musicSource)
         {
             source.mute = !source.mute;
-            Debug.Log(source.mute);
         }
     }
 
+    // Toggle sound effect playback
     public void ToggleSFX()
     {
         foreach (AudioSource source in sfxSource)
@@ -171,6 +197,7 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    // Change music volume
     public void ChangeMusicVolume(float volume)
     {
         _musicVolume = volume;
@@ -180,6 +207,7 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    // Change sound effect volume
     public void ChangeSFXVolume(float volume)
     {
         _sfxVolume = volume;
@@ -189,11 +217,13 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    // Get current music volume
     public float GetMusicVolume()
     {
         return _musicVolume;
     }
 
+    // Get current sound effect volume
     public float GetSFXVolume()
     {
         return _sfxVolume;
